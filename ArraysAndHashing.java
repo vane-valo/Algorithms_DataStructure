e88a8f1fe4d8dd1c50cbb494862c31da94de1335
Vanessa Vaca <vanessa.vaca96@gmail.com>
2025-08-14 16:51:41 -0600

    Two Sum Algorithm

--- ArraysAndHashing.java
+++ ArraysAndHashing.java
@@ -64,7 +64,7 @@ public class ArraysAndHashing{
             int[] result = new int[2];
 
             for (int i = 0; i < nums.length; i++) {
-                for (int j = 1; j < nums.length; j++) {
+                for (int j = i + 1; j < nums.length; j++) {
                     if (nums[i] + nums[j] == target) {
                         result[0] = j;
                         result[1] = i;
